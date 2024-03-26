d10196e6c0eca476869a96083e7d7286a1b55524
rubythalib33 <rubyabdullah14@gmail.com>
2024-03-26 11:40:35 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # facenet-knowledge-distillation
+
+I train knowledge distillation from open source face recognition models called facenet-pytorch
+
+https://github.com/timesler/facenet-pytorch
+
+Trained Done:
+- MobileNetV2 models
+
+## Benchmark
+```
+Device: CPU
+InceptionResnetV1: Parameters = 27910327, Inference Time = 0.05453 seconds
+EfficientNetB1: Parameters = 7794184, Inference Time = 0.02678 seconds
+MobileNetV3-Large: Parameters = 5483032, Inference Time = 0.01192 seconds
+MobileNetV2: Parameters = 3504872, Inference Time = 0.01510 seconds
+
+Device: CUDA
+InceptionResnetV1: Parameters = 27910327, Inference Time = 0.01620 seconds
+EfficientNetB1: Parameters = 7794184, Inference Time = 0.01093 seconds
+MobileNetV3-Large: Parameters = 5483032, Inference Time = 0.00557 seconds
+MobileNetV2: Parameters = 3504872, Inference Time = 0.00481 seconds
+```
---
+++ predict.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn.functional as F
+from torchvision import transforms
+from facenet_pytorch import MTCNN
+from PIL import Image
+import models
+from config import MODEL
+
+def load_and_crop_image(image_path, detector, device):
+    image = Image.open(image_path)
+    # Use MTCNN to find faces in the image and get the first detected face
+    boxes, _ = detector.detect(image)
+    if boxes is not None:
+        # Assuming the first box is the target face
+        box = boxes[0]
+        # Convert box from numpy array to a list with correct order
+        box = [int(b) for b in box.tolist()]
+        # Crop the image to the face
+        cropped_image = image.crop(box)
+        # Convert the PIL Image to a tensor
+        transform = transforms.Compose([
+            transforms.Resize((160, 160)),
+            transforms.ToTensor(),
+        ])
+        cropped_image = transform(cropped_image).unsqueeze(0).to(device)
+        return cropped_image
+    else:
+        print(f"No face detected in {image_path}.")
+        return None
+
+def cosine_similarity(embedding1, embedding2):
+    return F.cosine_similarity(embedding1, embedding2)
+
+def predict(image_path1, image_path2, student_model):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    # Initialize MTCNN
+    mtcnn = MTCNN(keep_all=False, device=device)
+    
+    # Load and process images
+    image1 = load_and_crop_image(image_path1, mtcnn, device)
+    image2 = load_and_crop_image(image_path2, mtcnn, device)
+    
+    # Ensure both images were successfully processed
+    if image1 is not None and image2 is not None:
+        # Get embeddings from the student model
+        embedding1 = student_model(image1)
+        embedding2 = student_model(image2)
+        
+        # Calculate cosine similarity
+        similarity = cosine_similarity(embedding1, embedding2)
+        print(f"Cosine similarity: {similarity.item()}")
+    else:
+        print("One or both images could not be processed.")
+
+if __name__ == "__main__":
+    # Load the student model
+    student_model = models.get_model(MODEL).eval()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    student_model.load_state_dict(torch.load('ckpt/mobilenet_v2_adam.pt'))
+    student_model.to(device)
+    
+    # Example usage
+    # image_path1 = 'sample/elon1.webp'
+    image_path1 = 'sample/jeff2.jpg'
+    # image_path2 = 'sample/elon2.webp'
+    image_path2 = 'sample/jeff1.jpeg'
+    predict(image_path1, image_path2, student_model)
